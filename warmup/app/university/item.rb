03b288ad529476175015fd94a8be9238a65c4676
Urs Gerber <ug.gerber@gmail.com>
2012-09-24 22:42:21 +0200

    Added first lines of code
    	modified:   warmup/.idea/workspace.xml
    	modified:   warmup/app/university/item.rb
    	modified:   warmup/app/university/user.rb

--- warmup/app/university/item.rb
+++ warmup/app/university/item.rb
@@ -1,3 +1,18 @@
-class Item
-  # To change this template use File | Settings | File Templates.
+module University
+  class Item
+    attr_accessor :name, :price, :owner, :active
+
+    def initialize
+      self.active = false
+      self.owner = nil
+    end
+
+    def self.named_with_price(name, price)
+      item = Item.new
+      item.name = name
+      item.price = price
+      return item
+    end
+
+  end
 end
--- warmup/app/university/user.rb
+++ warmup/app/university/user.rb
@@ -1,3 +1,23 @@
-class User
-  # To change this template use File | Settings | File Templates.
+module University
+  class User
+
+    attr_accessor :name, :credits, :items
+
+    def initialize
+      self.credits = 100
+      self.items = Array.new
+    end
+
+    def self.named(name)
+      user = User.new
+      user.name = name
+
+      return name
+    end
+
+    def add_item(item)
+      self.items.push(item)
+    end
+
+  end
 end
