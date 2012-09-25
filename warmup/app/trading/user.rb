34ba44a08a399a25953eefd4c27c491a818ce760
Urs Gerber <ug.gerber@gmail.com>
2012-09-25 14:09:47 +0200

    Added ability to buy items from other users
    	modified:   warmup/app/trading/user.rb
    	modified:   warmup/test/user_test.rb

--- warmup/app/trading/user.rb
+++ warmup/app/trading/user.rb
@@ -17,25 +17,47 @@ module Trading
     def create_item(name, price)
       item = Item.named_priced_with_owner(name, price, self)
       self.items.push(item)
+      return item
     end
 
     def show_active_items
-      out_string = ""
-      self.items.each { |item| out_string < "#{item}\n"}
+      active_items = Array.new
+      self.items.each do |item|
+        if (item.active)
+          active_items.push(item)
+        end
+      end
 
-      return out_string
+      return active_items
     end
 
     def add_item(item)
       self.items.push(item)
+      item.owner = self
     end
 
-    def take_credits(amount)
-      self.credits -= amount
+    def remove_item(item)
+      if (self.items.include? item)
+        item.owner = nil
+        self.items.delete(item)
+      end
     end
 
-    def give_credits(amount)
-      self.credits += amount
+    def buy_item(item)
+      # check for moneys and activity first
+      seller = item.owner
+
+      if (!seller.nil?)
+        seller.remove_item(item)
+        seller.credits += item.price
+      end
+
+      item.owner = self
+      item.active = false
+      self.add_item(item)
+      self.credits -= item.price
+
+      # maybe return boolean for success
     end
 
     def to_s
--- warmup/test/user_test.rb
+++ warmup/test/user_test.rb
@@ -6,12 +6,9 @@ class UserTest < Test::Unit::TestCase
   def test_user
     user = Trading::User.named("Hans")
     puts user.to_s
-    user.give_credits(20)
-    user.create_item("Test Item", 500);
-    puts user.items[0].to_s
-    puts user.show_active_items
-
-    assert(user.credits == 120)
-    assert(user.credits == 120)
+    item = user.create_item("Test Item", 500);
+    active_items = user.show_active_items
+    item.active = true
+    active_items = user.show_active_items
   end
 end
